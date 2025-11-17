6802e8d415e8ba88c7422082bcab1a0d236d35b3
Paul Muller <pamuller@z2r10p2.42lyon.fr>
2025-11-17 14:38:46 +0100

    removed unwanted comments

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,23 +6,11 @@
 /*   By: pamuller <pamuller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 17:34:38 by pamuller          #+#    #+#             */
-/*   Updated: 2025/11/17 12:03:01 by pamuller         ###   ########.fr       */
+/*   Updated: 2025/11/17 13:56:59 by pamuller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "ft_printf.h"
 
 int	ft_putchar_fd(char c, int fd)
 {
 	return (write(fd, &c, 1));
 }
-
-/*
-#include <fcntl.h>
-int	main(void)
-{
-	int	fd;
-
-	fd = open("txt", O_WRONLY);
-	ft_putchar_fd('c', fd);
-}*/
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: pamuller <pamuller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/06 17:42:52 by pamuller          #+#    #+#             */
-/*   Updated: 2025/11/17 12:42:47 by pamuller         ###   ########.fr       */
+/*   Updated: 2025/11/17 14:38:19 by pamuller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,3 @@ int	ft_putstr_fd(char *s, int fd)
 		return (write(fd, "(null)", 6));
 	return (write (fd, s, ft_strlen(s)));
 }
-
-// #include <fcntl.h>
-// int	main(void)
-// {
-// 	int	fd;
-
-// 	fd = open(NULL, O_WRONLY);
-// 	ft_putstr_fd("Bonjour a tous", fd);
-// }
